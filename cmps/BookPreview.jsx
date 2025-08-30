dd78b1067e6efc45d6394235129faceb32375370
VadimKre <vkreslavsky@gmail.com>
2025-08-30 22:58:47 +0300

    finished css for the preview cards

--- cmps/BookPreview.jsx
+++ cmps/BookPreview.jsx
@@ -4,11 +4,31 @@ const { useState, useEffect, useRef} = React
 
 export function BookPreview({ book }) {
 
+    const [hover, setHover] = useState(false)
+
+    const elTitleRef = useRef()
+
+    useEffect( () => {
+
+        hover ? elTitleRef.current.style.color = 'var(--mb-primary)' : elTitleRef.current.style.color = 'var(--mb-honey)'
+    }, [hover])
+
     return (
-        <div className='book-preview-container'>
-            <h3 className='book-preview-title'>{ book.title }</h3>
-            <BookDescription desc={book.description} numOfCharToDisplay={70}/>
-            <p className='book-preview-authors'>{ book.authors }</p>
+        <div 
+            className='book-preview-container'
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}>
+            <h3 
+                ref={elTitleRef} 
+                className='book-preview-title'>
+                    { book.title }
+            </h3>
+            <BookDescription 
+                className='book-preview-description' 
+                desc={book.description} 
+                numOfCharToDisplay={110}
+            />
+            <p className='book-preview-authors'>By: { book.authors }</p>
         </div>
     )
 }
